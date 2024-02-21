23a3e9c9795562fb34d967b9363970b98055dded
Harsh Bairagi <hvbairagi@gmail.com>
2024-02-21 10:19:58 +0530

    double click working after help from github (aman)

--- 12 double click/script.js
+++ 12 double click/script.js
@@ -1,13 +1,8 @@
 const blinker = document.getElementsByClassName("blinker")[0];
 
 function doubleClickHandler(e) {
-  let X = document.documentElement.clientWidth;
-  X = (X-500)/2;
-  const Y = document.documentElement.clientHeight;
-  const x = e.clientX,
-    y = e.clientY;
-  console.log(X, x, x+X);
-  // console.log(Y, y, Y - y);
+  const x = e.clientX - e.target.offsetLeft,
+    y = e.clientY - e.target.offsetTop;
   blinker.style.left = `${x}px`;
   blinker.style.top = `${y}px`;
   blinker.classList.add("active");
