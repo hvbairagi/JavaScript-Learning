9ffe11bf4a52cc73ce4d24e344303e4ba0b8bcce
Harsh Bairagi <hvbairagi@gmail.com>
2024-02-21 11:22:57 +0530

    Currency formatter completed

--- 10 currency/script.js
+++ 10 currency/script.js
@@ -1,8 +1,50 @@
 const input = document.querySelector("input");
 const result = document.getElementById("result");
+const dropdown = document.getElementById("currency");
 
 function submitHandler(event) {
   event.preventDefault();
   let amount = +input.value;
-amount.
+  const currency = dropdown.value;
+  switch (currency) {
+    case "INR":
+      reformatINR(amount);
+      break;
+    case "USD":
+      reformatUSD(amount);
+      break;
+    case "EUR":
+      reformatEUR(amount);
+      break;
+    default:
+      alert("Choose a currency.");
+      break;
+  }
+}
+
+/**
+ * @param {number} amount
+ */
+function reformatINR(amount) {
+  const options = { style: "currency", currency: "INR" };
+  const res = amount.toLocaleString("en-IN", options);
+  result.innerText = res;
+}
+
+/**
+ * @param {number} amount
+ */
+function reformatUSD(amount) {
+  const options = { style: "currency", currency: "USD" };
+  const res = amount.toLocaleString("en-US", options);
+  result.innerText = res;
+}
+
+/**
+ * @param {number} amount
+ */
+function reformatEUR(amount) {
+  const options = { style: "currency", currency: "EUR" };
+  const res = amount.toLocaleString("en-GB", options);
+  result.innerText = res;
 }
