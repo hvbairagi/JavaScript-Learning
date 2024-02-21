87159648d23af2b84e16194a9004ff9641a21df9
Harsh Bairagi <hvbairagi@gmail.com>
2024-02-21 10:25:11 +0530

    center the circle to mouse click

--- 12 double click/script.js
+++ 12 double click/script.js
@@ -3,8 +3,8 @@ const blinker = document.getElementsByClassName("blinker")[0];
 function doubleClickHandler(e) {
   const x = e.clientX - e.target.offsetLeft,
     y = e.clientY - e.target.offsetTop;
-  blinker.style.left = `${x}px`;
-  blinker.style.top = `${y}px`;
+  blinker.style.left = `${x-8}px`;
+  blinker.style.top = `${y-8}px`;
   blinker.classList.add("active");
   setTimeout(() => {
     blinker.classList.remove("active");
