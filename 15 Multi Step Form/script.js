9a044d3c11c3889b605b04344963a39888d4bc19
Harsh Vardhan Bairagi <54072364+hvbairagi@users.noreply.github.com>
2024-03-06 13:01:57 +0530

    Multi Step form - Added button disable logic

--- 15 Multi Step Form/script.js
+++ 15 Multi Step Form/script.js
@@ -1,23 +1,36 @@
 let currentStep = 1;
 const steps = document.body.getElementsByClassName("number");
 const activeSteps = [steps[0]];
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
 
 function prev() {
   if (currentStep > 1) {
     currentStep = currentStep - 1;
+    disableButton();
     const removedStep = activeSteps.pop();
     removedStep.classList.remove("active");
   }
-  animate();
 }
 
 function next() {
   if (currentStep > 0 && currentStep < 5) {
     activeSteps.push(steps[currentStep]);
     currentStep = currentStep + 1;
+    disableButton();
     activeSteps[activeSteps.length - 1].classList.add("active");
   }
-  animate();
 }
 
-function animate() {}
+function disableButton() {
+  if (currentStep === 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+  }
+  if (currentStep === 5) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+  }
+}
